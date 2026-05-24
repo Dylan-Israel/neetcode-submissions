27733aaf0422526b33167fcd8b2a291b232b3fda
Dylan C. Israel <dylansemail310@gmail.com>
2026-05-24 18:21:52 -0400

    Add: top-k-elements-in-list - submission-8

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-8.ts
@@ -0,0 +1,47 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        // Guard
+        if(nums.length === 0) {
+            return [];
+        }
+
+        // Count Map
+        const numsCountMap = nums.reduce((acc, value) => {
+            if(acc.has(value)) {
+                acc.set(value, acc.get(value) + 1);
+            } else {
+                acc.set(value, 1);
+            }
+            return acc;
+        }, new Map<number, number>());
+     
+        const buckets: number[][] = [];
+
+        numsCountMap.forEach((count, key) => {
+            if(buckets[count]) {
+                buckets[count].push(key);
+            } else {
+                buckets[count] = [key];
+            }
+        })
+
+        const answer = [];
+
+        for(let i = buckets.length -1; i > 0; i--){
+            if(answer.length === k) {
+                break;
+            }
+
+            if(buckets[i]) {
+                answer.push(...buckets[i]);
+            }
+        }
+ 
+        return answer;
+    }
+}
