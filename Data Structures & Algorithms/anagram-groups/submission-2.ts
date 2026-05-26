be1be5271e4e5f6a3e64945e1b86b671565f9879
Dylan C. Israel <dylansemail310@gmail.com>
2026-05-26 10:07:10 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.ts
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        // Guard
+        if(!strs.length) {
+            return [];
+        }
+
+        const response = {};
+
+        for(const str of strs) {
+            // make array for bucket sort key
+            const chars = new Array(26).fill(0);
+
+            for(const char of str) {
+                const charIndex = char.charCodeAt(0) - 'a'.charCodeAt(0);
+                chars[charIndex]++; 
+            }
+
+            const key = chars.join(',');
+
+            if(response[key]) {
+                response[key].push(str);
+            } else {
+                response[key] = [str];
+            }
+        }
+ 
+        return Object.values(response);
+    }
+}
