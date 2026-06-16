b9025cc9a8dc250b2475cee98b588af5c67f8670
Dylan C. Israel <dylansemail310@gmail.com>
2026-06-16 19:24:28 -0400

    Add: reverse-string - submission-0

---
+++ Data Structures & Algorithms/reverse-string/submission-0.ts
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {character[]} s
+     * @return {void} Do not return anything, modify s in-place instead.
+     */
+    reverseString(s: string[]): void {
+        for(let i =0, j = s.length -1; i<j; i++, j--) {
+            const strOfI = s[i];
+            const strOfJ = s[j];
+
+            s[i] = strOfJ;
+            s[j] = strOfI;
+        }
+    }
+}
