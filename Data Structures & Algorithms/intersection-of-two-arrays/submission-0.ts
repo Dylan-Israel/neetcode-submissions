af02b76f58a6b4222c162a36ddfe7b8a1d104de3
Dylan C. Israel <dylansemail310@gmail.com>
2026-06-23 16:05:23 -0400

    Add: intersection-of-two-arrays - submission-0

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-0.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number[]} nums2
+     * @return {number[]}
+     */
+    intersection(nums1: number[], nums2: number[]): number[] {
+        const set1 = new Set(nums1);
+        const set2 = new Set(nums2);
+        const result = [];
+
+        for (const num of set1) {
+            if (set2.has(num)) {
+                result.push(num);
+            }
+        }
+
+        return result;
+    }
+}
