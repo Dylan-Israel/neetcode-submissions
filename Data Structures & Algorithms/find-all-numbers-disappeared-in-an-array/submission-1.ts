9211c4781c6e048c774521febe298dd35f891b26
Dylan C. Israel <dylansemail310@gmail.com>
2026-07-06 13:31:41 -0400

    Add: find-all-numbers-disappeared-in-an-array - submission-1

---
+++ Data Structures & Algorithms/find-all-numbers-disappeared-in-an-array/submission-1.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    findDisappearedNumbers(nums: number[]): number[] {
+        const missingNums = [];
+        const numsMap = nums.reduce((acc, value) => {
+            if (acc[value] === undefined) {
+                acc[value] = value;
+            }
+
+            return acc;
+        }, {});
+
+        for (let i = 1; i <= nums.length; i++) {
+            if (numsMap[i] === undefined) {
+                missingNums.push(i);
+            }
+        }
+
+        return missingNums;
+    }
+}
